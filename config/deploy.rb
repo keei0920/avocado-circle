09dd4d79b152cad2d7220799c28ba835202b40d8
github keei0920 <yellowsubmarine.0920@gmail.com>
2023-11-22 13:04:56 +0900

    自動デプロイの設定

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+# capistranoのバージョンを記載
+lock '3.18.0'
+
+# Capistranoのログの表示に利用する
+set :application, 'avocado-circle'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:keei0920/avocado-circle.git'
+set :branch, 'main'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0' #カリキュラム通りに進めた場合、’3.2.0’ です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/0920reei.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
