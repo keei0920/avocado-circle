0bd50ce291ddb22a254250625308b063076e048c
github keei0920 <yellowsubmarine.0920@gmail.com>
2023-11-25 22:59:25 +0900

    ユーザー登録バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :purpose
+
+  validates :nickname, presence: true, uniqueness: true,length: { maximum: 10 }
   
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :purpose_id
+    validates :prefecture_id
+  end   
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
 end
