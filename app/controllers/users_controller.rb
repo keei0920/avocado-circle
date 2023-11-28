5903d9e9b2c2d1478eeb4e46b22a0628c1e9f880
github keei0920 <yellowsubmarine.0920@gmail.com>
2023-11-28 17:49:24 +0900

    ユーザー情報編集ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to users_show_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :purpose_id, :prefecture_id, :introduce)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: "posts#index"
+  resources :users, only: [:edit, :update]
 end
