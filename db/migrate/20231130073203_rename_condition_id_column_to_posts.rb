f9041f5d90b378377b8f33e8f97b6cae1c1fbcd0
github keei0920 <yellowsubmarine.0920@gmail.com>
2023-11-30 21:38:32 +0900

    不要なマイグレーションファイルの削除

--- db/migrate/20231130073203_rename_condition_id_column_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class RenameConditionIdColumnToPosts < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :posts, :condition_id, :condition
-  end
-end
--- db/migrate/20231130080130_rename_condition_column_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class RenameConditionColumnToPosts < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :posts, :condition, :condition_id
-  end
-end
