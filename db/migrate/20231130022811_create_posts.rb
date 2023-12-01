7289bb4599feb4beeb0736fd8742dccb0ed327ab
github keei0920 <yellowsubmarine.0920@gmail.com>
2023-12-01 20:05:12 +0900

    デプロイ状態の改善２

--- db/migrate/20231130022811_create_posts.rb
+++ db/migrate/20231130022811_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.date       :date,             null: false
+      t.date       :date,            null: false
       t.integer    :condition_id,     null: false
       t.text       :text            
       t.references :user,            foreign_key: true
