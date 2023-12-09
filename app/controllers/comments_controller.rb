fbc29efcff117f03807822943613169120c1c976
github keei0920 <yellowsubmarine.0920@gmail.com>
2023-12-09 15:20:51 +0900

    コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = Comment.find(params[:post_id])
+    comment.destroy
+    redirect_to post_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
