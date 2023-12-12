101bfad72a2e2efb353bc0f7ebbef58e3cfbf00c
github keei0920 <yellowsubmarine.0920@gmail.com>
2023-12-12 22:50:05 +0900

    いいね機能の非同期通信化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
     like = current_user.likes.build(post_id: params[:post_id])
     like.save
-    redirect_to root_path
+    render partial: 'likes/like', locals: { post: @post }
   end
 
   def destroy
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    render partial: 'likes/like', locals: { post: @post }
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
