af065fd17b9db855321a301dd6babf531018ca67
github keei0920 <yellowsubmarine.0920@gmail.com>
2023-12-16 15:20:07 +0900

    ゲストログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: "posts#index"
+<<<<<<< Updated upstream
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
+=======
+  post '/posts/guest_sign_in', to: 'posts#guest_sign_in'
+>>>>>>> Stashed changes
   resources :users, only: [:show, :edit, :update]
   resources :avocados, only: [:new, :create, :edit, :update]
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
